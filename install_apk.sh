03bac0fbf76c31c62b1eaa9472412801b084cdd2
OnelifeOnedeath <137838121+OnelifeOnedeath@users.noreply.github.com>
2025-11-06 15:42:18 +0300

    Delete install_apk.sh

--- install_apk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Установка мобильного обхода..."
-pkg update && pkg install python -y
-pip install requests
-git clone https://github.com/OnelifeOnedeath/Zepret-White-List
-cd Zepret-White-List
-python mobile_proxy.py &
-echo "Туннель запущен. Настройте прокси в WiFi на IP этого устройства"
